a4f6e8d716f04a775dff4f28ff637709e707ad40
Jan Erik Zassenhaus <sisko1990@users.sourceforge.net>
2009-07-09 12:35:18 +0000

    Beginn der Erstellung einer neutralen Frontend-Übersetzung

---
+++ language/de-DE/de-DE.ignore.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @version		$Id: de-DE.ignore.php 4 2009-07-07 21:04:54Z sisko1990 $
+* @package		Joomla
+* @copyright	Copyright (C) 2005 - 2009 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+$search_ignore[] = "und";
+$search_ignore[] = "in";
+$search_ignore[] = "am";
+$search_ignore[] = "auf";
+$search_ignore[] = "von";
+$search_ignore[] = "vom";
+$search_ignore[] = "mit";
+$search_ignore[] = "admin";
+?>

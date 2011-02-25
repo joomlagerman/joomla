2b5f8c6626213ee33219314d42b36debd2dcfd45
Jan Erik Zassenhaus <sisko1990@users.sourceforge.net>
2011-02-25 15:10:50 +0000

    Anpassungen an neue Paketvorgaben von joomla.org

---
+++ administrator/language/de-DE/de-DE.localise.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @version		$Id: de-DE.localise.php 267 2010-12-27 06:16:55Z sisko1990 $
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * de-DE localise class
+ *
+ * @package		Joomla.Site
+ * @since		1.6
+ */
+abstract class de_DELocalise {
+	/**
+	 * Returns the potential suffixes for a specific number of items
+	 *
+	 * @param	int $count  The number of items.
+	 * @return	array  An array of potential suffixes.
+	 * @since	1.6
+	 */
+	public static function getPluralSuffixes($count) {
+		if ($count == 0) {
+			$return =  array('0');
+		}
+		elseif($count == 1) {
+			$return =  array('1');
+		}
+		else {
+			$return = array('MORE');
+		}
+		return $return;
+	}
+	/**
+	 * Returns the ignored search words
+	 *
+	 * @return	array  An array of ignored search words.
+	 * @since	1.6
+	 */
+	public static function getIgnoredSearchWords() {
+		$search_ignore = array();
+		$search_ignore[] = "und";
+		$search_ignore[] = "in";
+		$search_ignore[] = "auf";
+		return $search_ignore;
+	}
+	/**
+	 * Returns the lower length limit of search words
+	 *
+	 * @return	integer  The lower length limit of search words.
+	 * @since	1.6
+	 */
+	public static function getLowerLimitSearchWord() {
+		return 3;
+	}
+	/**
+	 * Returns the upper length limit of search words
+	 *
+	 * @return	integer  The upper length limit of search words.
+	 * @since	1.6
+	 */
+	public static function getUpperLimitSearchWord() {
+		return 20;
+	}
+	/**
+	 * Returns the number of chars to display when searching
+	 *
+	 * @return	integer  The number of chars to display when searching.
+	 * @since	1.6
+	 */
+	public static function getSearchDisplayedCharactersNumber() {
+		return 200;
+	}
+}

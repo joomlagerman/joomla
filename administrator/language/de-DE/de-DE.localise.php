20095aa0cf3a62f9e3f7bb924045a8b0b76e4992
Jan Erik Zassenhaus <Jan.Zassenhaus@jgerman.de>
2013-09-15 13:55:51 +0200

    Aktualisierung nach Joomla-CMS-GIT bis joomla/joomla-cms@d81aa627dc5be1ba3fba417ff9ed94860cb49c20

--- administrator/language/de-DE/de-DE.localise.php
+++ administrator/language/de-DE/de-DE.localise.php
@@ -19,28 +19,34 @@ abstract class De_DELocalise
 	/**
 	 * Returns the potential suffixes for a specific number of items
 	 *
-	 * @param	int $count  The number of items.
-	 * @return	array  An array of potential suffixes.
-	 * @since	1.6
+	 * @param   integer  $count  The number of items.
+	 *
+	 * @return  array  An array of potential suffixes.
+	 *
+	 * @since   1.6
 	 */
 	public static function getPluralSuffixes($count)
 	{
-		if ($count == 0) {
-			$return = array('0');
+		if ($count == 0)
+		{
+			return array('0');
 		}
-		elseif ($count == 1) {
-			$return = array('1');
+		elseif ($count == 1)
+		{
+			return array('1');
 		}
-		else {
-			$return = array('MORE');
+		else
+		{
+			return array('MORE');
 		}
-		return $return;
 	}
+
 	/**
 	 * Returns the ignored search words
 	 *
-	 * @return	array  An array of ignored search words.
-	 * @since	1.6
+	 * @return  array  An array of ignored search words.
+	 *
+	 * @since   1.6
 	 *
 	 * @source http://solariz.de/649/deutsche-stopwords.htm
 	 */
@@ -1045,34 +1051,40 @@ abstract class De_DELocalise
     $search_ignore[] = "übrigens";
 		return $search_ignore;
 	}
+
 	/**
 	 * Returns the lower length limit of search words
 	 *
-	 * @return	integer  The lower length limit of search words.
-	 * @since	1.6
+	 * @return  integer  The lower length limit of search words.
+	 *
+	 * @since   1.6
 	 */
 	public static function getLowerLimitSearchWord()
-  {
+	{
 		return 3;
 	}
+
 	/**
 	 * Returns the upper length limit of search words
 	 *
-	 * @return	integer  The upper length limit of search words.
-	 * @since	1.6
+	 * @return  integer  The upper length limit of search words.
+	 *
+	 * @since   1.6
 	 */
 	public static function getUpperLimitSearchWord()
-  {
+	{
 		return 20;
 	}
+
 	/**
 	 * Returns the number of chars to display when searching
 	 *
-	 * @return	integer  The number of chars to display when searching.
-	 * @since	1.6
+	 * @return  integer  The number of chars to display when searching.
+	 *
+	 * @since   1.6
 	 */
 	public static function getSearchDisplayedCharactersNumber()
-  {
+	{
 		return 200;
 	}
 }
--- media/editors/tinymce/jscripts/tiny_mce/plugins/autosave/langs/de.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addI18n('de.autosave',{
-restore_content: "Automatisch gespeicherten Stand wiederherstellen",
-warning_message: "Wenn der automatisch gespeicherte Stand wiederhergestellt wird, geht der aktuelle Inhalt des Editors verlohren!\n\nSicher, den Stand jetzt wiederherzustellen?"
-});

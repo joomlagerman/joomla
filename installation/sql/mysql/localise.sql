8c90a203dddaa78537f1e28917d13942221c2f08
Jan Erik Zassenhaus <sisko1990@users.noreply.github.com>
2016-05-14 22:03:38 +0200

    Vorbereitung reines de-DE Sprachpaket

--- installation/sql/mysql/localise.sql
+++ installation/sql/mysql/localise.sql
@@ -8,8 +8,8 @@
 UPDATE IGNORE `#__extensions` SET `params` = REPLACE(`params`, 'en-GB', 'de-DE') WHERE `extension_id` = 11;
 
 INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `client_id`, `access`, `manifest_cache`) VALUES
-(602, 'German (DE-CH-AT)', 'language', 'de-DE', 0, 1, '{"name":"German (Germany-Switzerland-Austria)","type":"language","creationDate":"06.04.2016","author":"J!German","copyright":"Copyright (C) 2005 - 2016 Open Source Matters & J!German. All rights reserved.","authorEmail":"team@jgerman.de","authorUrl":"http:\/\/www.jgerman.de","version":"3.5.1.1","group":""}'),
-(603, 'German (DE-CH-AT)', 'language', 'de-DE', 1, 1, '{"name":"German (Germany-Switzerland-Austria)","type":"language","creationDate":"06.04.2016","author":"J!German","copyright":"Copyright (C) 2005 - 2016 Open Source Matters & J!German. All rights reserved.","authorEmail":"team@jgerman.de","authorUrl":"http:\/\/www.jgerman.de","version":"3.5.1.1","group":""}'),
+(602, 'German (DE)', 'language', 'de-DE', 0, 1, '{"name":"German (Germany)","type":"language","creationDate":"06.04.2016","author":"J!German","copyright":"Copyright (C) 2005 - 2016 Open Source Matters & J!German. All rights reserved.","authorEmail":"team@jgerman.de","authorUrl":"http:\/\/www.jgerman.de","version":"3.5.1.1","group":""}'),
+(603, 'German (DE)', 'language', 'de-DE', 1, 1, '{"name":"German (Germany)","type":"language","creationDate":"06.04.2016","author":"J!German","copyright":"Copyright (C) 2005 - 2016 Open Source Matters & J!German. All rights reserved.","authorEmail":"team@jgerman.de","authorUrl":"http:\/\/www.jgerman.de","version":"3.5.1.1","group":""}'),
 (604, 'German Language Pack', 'package', 'pkg_de-DE', 0, 1, '{"name":"German Language Pack","type":"package","creationDate":"06.04.2016","author":"J!German","copyright":"","authorEmail":"team@jgerman.de","authorUrl":"http:\/\/www.jgerman.de","version":"3.5.1.1","group":"","filename":"pkg_de-DE"}');
 
 --

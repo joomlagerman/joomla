31b26a1c412524da705501e6b058ef1143b65f89
Christian Heel <66922325+heelc29@users.noreply.github.com>
2022-08-17 21:13:47 +0200

    remove mod_status id:14
    joomla/joomla-cms#25570

--- installation/sql/mysql/localise.sql
+++ installation/sql/mysql/localise.sql
@@ -57,7 +57,6 @@ UPDATE IGNORE `#__modules` SET `title` = 'Symbolleiste' WHERE `id` = 8;
 UPDATE IGNORE `#__modules` SET `title` = 'Benachrichtigungen' WHERE `id` = 9;
 UPDATE IGNORE `#__modules` SET `title` = 'Angemeldete Benutzer' WHERE `id` = 10;
 UPDATE IGNORE `#__modules` SET `title` = 'Admin Menü' WHERE `id` = 12;
-UPDATE IGNORE `#__modules` SET `title` = 'Benutzerstatus' WHERE `id` = 14;
 UPDATE IGNORE `#__modules` SET `title` = 'Titel' WHERE `id` = 15;
 UPDATE IGNORE `#__modules` SET `title` = 'Anmeldeformular' WHERE `id` = 16;
 UPDATE IGNORE `#__modules` SET `title` = 'Navigationspfad' WHERE `id` = 17;

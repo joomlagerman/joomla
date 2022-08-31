e95e5fe75ab6426898e15bf467502239c1e6fd2a
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2022-08-31 09:15:55 +0200

    fix #2567

--- language/de-DE/localise.php
+++ language/de-DE/localise.php
@@ -2,12 +2,18 @@
 /**
  * @package    Joomla.Language
  *
- * @copyright  (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright  (C) 2011 Open Source Matters, Inc. <https://www.joomla.org>
  * @copyright  (C) Translation 2008 - 2022 J!German <https://www.jgerman.de>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
+
+ * @phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
+
+ * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
  */
 
-defined('_JEXEC') or die;
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
 
 /**
  * de-DE localise class.

b8620a97a71b0c2a1422920713f6af41df49f83e
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2023-09-05 10:29:17 +0200

    resolve conflict

--- script.php
+++ script.php
@@ -410,6 +410,8 @@ class Pkg_deDEInstallerScript extends InstallerScript
 			'/administrator/language/de-DE/plg_fields_image.sys.ini',
 			'/administrator/language/de-DE/plg_fields_menuitem.ini',
 			'/administrator/language/de-DE/plg_fields_menuitem.sys.ini',
+			'/administrator/language/de-DE/plg_system_logrotation.ini',
+			'/administrator/language/de-DE/plg_system_logrotation.sys.ini',
 			'/administrator/language/de-DE/plg_system_updatenotification.ini',
 			'/administrator/language/de-DE/plg_system_updatenotification.sys.ini',
 			'/administrator/language/de-DE/plg_task_demotasks.ini',

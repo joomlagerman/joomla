6d6c23e16c37ad475009744e6fa1ad90dcd6bec6
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2024-04-11 11:52:33 +0200

    [5.1] Complete com_search deprecation #40727 (#3162)
    * fix #3161
    
    add deprecated message
    
    * fix

--- administrator/language/de-DE/localise.php
+++ administrator/language/de-DE/localise.php
@@ -49,6 +49,8 @@ abstract class De_DELocalise
 	 *
 	 * @since   1.6
 	 *
+	 * @deprecated  5.1 will be removed in 7.0 without replacement
+	 *
 	 * @link    https://solariz.de/649/deutsche-stopwords.htm
 	 */
 	public static function getIgnoredSearchWords()
@@ -1060,6 +1062,9 @@ abstract class De_DELocalise
 	 * @return  int  The lower length limit of search words.
 	 *
 	 * @since   1.6
+	 *
+	 * @deprecated  5.1 will be removed in 7.0 without replacement
+	 *
 	 */
 	public static function getLowerLimitSearchWord()
 	{
@@ -1072,6 +1077,9 @@ abstract class De_DELocalise
 	 * @return  int  The upper length limit of search words.
 	 *
 	 * @since   1.6
+	 *
+	 * @deprecated  5.1 will be removed in 7.0 without replacement
+	 *
 	 */
 	public static function getUpperLimitSearchWord()
 	{
@@ -1084,6 +1092,9 @@ abstract class De_DELocalise
 	 * @return  int  The number of chars to display when searching.
 	 *
 	 * @since   1.6
+	 *
+	 * @deprecated  5.1 will be removed in 7.0 without replacement
+	 *
 	 */
 	public static function getSearchDisplayedCharactersNumber()
 	{

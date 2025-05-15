48830a2d68fbcf11bf5a329c489b8ee335d58e32
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2025-05-15 21:56:16 +0200

    fix message

--- script.php
+++ script.php
@@ -37,7 +37,7 @@ return new class () implements InstallerScriptInterface {
      */
     public function install(InstallerAdapter $adapter): bool
     {
-        Factory::getApplication()->enqueueMessage(sprintf(Text::_('COM_INSTALLER_UNINSTALL_SUCCESS')), 'message');
+        Factory::getApplication()->enqueueMessage('Installation erfolgreich.');
 
         return true;
     }

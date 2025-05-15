0b813d30030c0c74608a98adacd04ebf0ae75acf
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2025-05-15 09:01:17 +0200

    fix #3467

--- script.php
+++ script.php
@@ -25,7 +25,7 @@ class Pkg_deDEInstallerScript extends InstallerScript
 	public function __construct()
 	{
 		// Define the minimum versions to be supported.
-		$this->minimumJoomla = '5.0';
+		$this->minimumJoomla = '6.0';
 		$this->minimumPhp    = '8.1.0';
 
 		$this->deleteFiles = [
@@ -423,9 +423,11 @@ class Pkg_deDEInstallerScript extends InstallerScript
 			'/administrator/language/de-DE/plg_twofactorauth_yubikey.ini',
 			'/administrator/language/de-DE/plg_twofactorauth_yubikey.sys.ini',
 			'/language/de-DE/lib_simplepie.sys.ini',
-			// Old files from Joomla 5 language packs (Only relevant for Joomla 6, should then be included in the deletion array with the 6.0-dev branch once created)
-			// '/administrator/language/de-DE/plg_captcha_recaptcha_invisible.ini',
-			// '/administrator/language/de-DE/plg_captcha_recaptcha_invisible.sys.ini',
+			// Old files from Joomla 5 language packs
+			'/administrator/language/de-DE/plg_captcha_recaptcha_invisible.ini',
+			'/administrator/language/de-DE/plg_captcha_recaptcha_invisible.sys.ini',
+            '/administrator/language/de-DE/plg_behaviour_compat.ini',
+            '/administrator/language/de-DE/plg_behaviour_compat.sys.ini',
 		];
 	}
 
